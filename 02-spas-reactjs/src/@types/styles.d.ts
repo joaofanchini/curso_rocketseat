adbd335736f8d1a3fe16460473fba421a10c4923
João Victor Fanchini de Azevedo <joaofanchini@gmail.com>
2023-01-27 02:29:31 -0300

    [02] Adicionando extensão para a tipagem do styled-components utilizada para adequação de propriedades

---
+++ 02-spas-reactjs/src/@types/styles.d.ts
@@ -0,0 +1,12 @@
+import 'styled-components';
+
+import {defaultTheme} from "../theme/default";
+
+type ThemeType = typeof defaultTheme;
+
+// Sobrescrevendo tipagem de uma biblioteca.
+// Neste caso fazemos com que as propriedades definidas pelo "defaultTheme" possam ser aplicadas
+// Na ‘interface’ já existente do DefaultTheme na biblioteca styled-components
+declare module 'styled-components' {
+    export interface DefaultTheme extends ThemeType {}
+}
