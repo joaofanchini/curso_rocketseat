5588a42617bbcac83b74454582b004c3f691ace6
João Victor Fanchini de Azevedo <joaofanchini@gmail.com>
2023-01-30 22:55:36 -0300

    [02] Adicionando no projeto o react-hooks-form, zod e @hookform/resolver para gerenciar os formulários utilizads, além de configurar o formulário dÃa página de Home

--- 02-spas-reactjs/src/router/pages/Home/Home.tsx
+++ 02-spas-reactjs/src/router/pages/Home/Home.tsx
@@ -1,5 +1,8 @@
-import {Play} from 'phosphor-react';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {useForm} from 'react-hook-form';
+import * as zod from 'zod'; // A biblioteca Zod não possui um export como default.
 
+import {Play} from 'phosphor-react';
 import {
     HomeContainer,
     TaskContainer,
@@ -11,28 +14,56 @@ import {
 } from './HomeStyles';
 
 const Home = () => {
+
+    const formInputs = zod.object({
+        task: zod.string()
+            .min(1, 'Nome de tarefa obrigatório.')
+            .max(25,'Nome de tarefa deve ter no máximo 25 caracteres.')
+        ,
+        minutesAmount: zod.number()
+            .min(5, 'a tarefa deve levar pelo menos 5 minutos.')
+            .max(60, 'A tarefa deve ter um máximo de 60 minutos.')
+    });
+
+    type TypeFormInput = zod.infer<typeof formInputs>
+
+    const {register, reset, watch, handleSubmit, formState} = useForm<TypeFormInput>({
+        resolver: zodResolver(formInputs),
+        defaultValues: {
+            task: '',
+            minutesAmount: 0
+        }
+    });
+
+    const watcherInputTask = watch('task');
+
+    const isSubmitDisabled = !watcherInputTask;
+
+    const createNewTimerEvent = (data: TypeFormInput) => {
+        console.log('submit', data);
+        reset();
+    };
+
     return <HomeContainer>
-        <form action=''>
+        <form onSubmit={handleSubmit(createNewTimerEvent)} action=''>
             <TaskContainer>
                 <label htmlFor='input_task'>Vou trabalhar em</label>
                 <TaskInput
-                    type='text'
                     id='input_task'
+                    type='text'
                     placeholder='Dê um nome para o seu projeto'
                     list='task-suggestions'
-                    required
+                    {...register('task')}
                 />
                 <datalist id='task-suggestions'>
                     <option value='Projeto'/>
                 </datalist>
                 <label htmlFor='minutes_amount'>durante</label>
                 <MinutesAmountInput
-                    type='number'
                     id='minutes_amount'
+                    type='number'
                     placeholder='00'
-                    step={5}
-                    min={5}
-                    max={60}
+                    {...register('minutesAmount', {valueAsNumber: true})}
                 />
                 <span>minutos.</span>
             </TaskContainer>
@@ -43,7 +74,7 @@ const Home = () => {
                 <span>0</span>
                 <span>0</span>
             </CountdownContainer>
-            <StartCountdownButton type='submit'>
+            <StartCountdownButton disabled={isSubmitDisabled} type='submit'>
                 <Play size={24}/> Começar
             </StartCountdownButton>
         </form>
